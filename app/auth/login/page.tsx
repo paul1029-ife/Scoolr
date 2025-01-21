97e4e5d403140190c71292a2adbccb3a7a9c5049
TheOne001 <paul.agbogun@gmail.com>
2025-01-21 13:55:51 +0100

    add Login Form component.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
-import React from "react";
+import { LoginForm } from "@/components/login-form";
 
-const LoginPage = () => {
+export default function Page() {
   return (
-    <div className="flex items-center justify-center w-full">
-      <Link href="/dashboard">Login</Link>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
   );
-};
-
-export default LoginPage;
+}
